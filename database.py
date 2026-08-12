93cef5a8f94b0b9663fea556e9c762c346cd49d5
Sergio Marcelo Hosan <marcelo.hosan@arba.gov.ar>
2026-08-12 16:59:40 -0300

    actu en database.py: importar ClientOptions y deshabilitar persistencia local de la sesion de supabase

--- database.py
+++ database.py
@@ -1,15 +1,18 @@
 # config.py
 import os
 from dotenv import load_dotenv
-from supabase import create_client, Client
+from supabase import create_client, Client, ClientOptions
 
 # Cargar variables de entorno
 load_dotenv()
 
-# Crear cliente (se ejecuta una sola vez al importar)
+# Crear cliente desactivando la persistencia de sesión local para evitar errores de bloqueo de archivos en Vercel Serverless
 supabase: Client = create_client(
     os.getenv("SUPABASE_URL"),
-    os.getenv("SUPABASE_KEY")
+    os.getenv("SUPABASE_KEY"),
+    options=ClientOptions(
+        persist_session=False
+    )
 )
 
 print("Conexion exitosa a Supabase")
